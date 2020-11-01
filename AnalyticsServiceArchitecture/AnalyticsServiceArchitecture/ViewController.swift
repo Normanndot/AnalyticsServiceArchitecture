47e0450946c5464a95df11d79c14ad281e68ac7e
Norman <normanndot@gmail.com>
2020-11-01 11:50:33 +0530

    Initial Commit

---
+++ AnalyticsServiceArchitecture/AnalyticsServiceArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnalyticsServiceArchitecture
+//
+//  Created by Norman, ThankaVijay on 01/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
